dc1fc910021f62bdc711cbad51e25487cf9accf6
Edis <vertazzar@gmail.com>
2020-04-03 00:43:58 +0200

    work

--- src/components/datepicker/demoCalendar/script.js
+++
@@ -1,5 +0,0 @@
-angular.module('calendarDemo', ['ngMaterial']).controller('AppCtrl', function() {
-  this.startDate = new Date();
-  this.endDate = new Date();
-  this.endDate.setDate(this.endDate.getDate() + 5);
-});
